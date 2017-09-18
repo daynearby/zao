215acade118a8637c13a6dcfbceda728686079a0
nearby <ff6220c@sina.com>
2017-09-18 22:36:37 +0800

    initalize repository

---
+++ app/src/main/java/com/hzu/zao/ZaoApplication.java
@@ -0,0 +1,22 @@
+package com.hzu.zao;
+
+import android.app.Application;
+
+/**
+ * Created by daynearby@hotmail.com on 17/9/18.
+ * note:
+ */
+public class ZaoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+
+
+}
