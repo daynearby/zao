0ccdf8ca610b6020d1adc36ca2e3f806dc52fc72
nearby <ff6220c@sina.com>
2017-09-19 00:25:06 +0800

    update config

---
+++ app/src/main/java/com/hzu/zao/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.hzu.zao.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * note:欢迎界面
+ * Created by daynearby@hotmail.com on 17/9/18.
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
